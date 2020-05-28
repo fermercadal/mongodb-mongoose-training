458ea03be3512f3a9d11537ba0073cfe5141b1fb
Fernando Mercadal <fermercadal@outlook.com>
2020-05-28 20:20:33 -0300

    Add schema

--- myApp.js
+++ myApp.js
@@ -41,6 +41,14 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // <Your code here >
 
+var Schema = mongoose.Schema;
+
+var   personSchema = new Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+
 // **Note**: Glitch is a real server, and in real servers interactions with
 // the db are placed in handler functions, to be called when some event happens
 // (e.g. someone hits an endpoint on your API). We'll follow the same approach
