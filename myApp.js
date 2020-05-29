86dfbe717da29de34878981c5926d4a3ff92df5d
Fernando Mercadal <fermercadal@outlook.com>
2020-05-29 20:27:29 -0300

    Remove by Id

--- myApp.js
+++ myApp.js
@@ -264,9 +264,10 @@ var findAndUpdate = function(personName, done) {
 // As usual, use the function argument `personId` as search key.
 
 var removeById = function(personId, done) {
-  
-  done(null/*, data*/);
-    
+  Person.findByIdAndRemove({_id: personId}, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 /** 11) Delete many People */
